0d10e873a3b186071b1f58405c83cbad47623f2f
Ashley Montine Sproul <montinesproul@gmail.com>
2023-03-28 19:13:36 -0400

    photos and readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Portfolio 
+
+## Description 
+This is a react application which displays photos of other applications, you are able to click on an icon to take you to the deployed site as well as one that will take you to the github repo. On the contact page you can send me an email right from the form. There is an About Me second as well as a downloadable resume. 
+
+
+[Deployed Application](https://montinesproul.github.io/portfolio_ams/)
+
+[GitHub Repo](https://github.com/MontineSproul/portfolio_ams)
+
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,27 @@ import '../css/form.css';
 
 
 
+
 function Home () {
     const images = [
         {
-           src: process.env.PUBLIC_URL + "images/apiQuiz.png",
+           src: process.env.PUBLIC_URL + "/images/apiQuiz.png",
            width: 320,
            height: 174,
-           description: document.getElementById('photo1')
+           customOverlay: (
+            <div className="custom-overlay__caption">
+              <div  className="custom-overlay__tag">
+              <a href="https://montinesproul.github.io/API_Quiz/" target="_blank" class="deployedSite"><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
+          </div>
+          <div  className="custom-overlay__tag">
+          <a href="https://github.com/MontineSproul/API_Quiz" target="_blank" class="githubRepo"><i className="fa-brands fa-github"></i></a>
+          </div>
+            </div>
+          ),
         //    caption: <i className="fa-solid fa-arrow-up-right-from-square"></i>,
         },
         {
-           src: process.env.PUBLIC_URL + "images/weathermann.png",
+           src: process.env.PUBLIC_URL + "/images/weathermann.png",
            width: 320,
            height: 212,
            alt: "Project",
@@ -46,11 +56,11 @@ function Home () {
         <div>
             <h2 className="portfolio">Portfolio</h2>
             <Gallery images={images} />
-            <div id="photo1">
-            {/* <Gallery images={images} /> */}
+            {/* <div id="photo1">
+            
                 <a href="https://montinesproul.github.io/API_Quiz/" target="_blank" class="deployedSite"><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
                 <a href="https://github.com/MontineSproul/API_Quiz" target="_blank" class="githubRepo"><i className="fa-brands fa-github"></i></a>
-                </div>
+                </div> */}
             {/* <div id="photo3">{images[2]}</div>
             <div id="photo4">{images[3]}</div>
             <div id="photo5">{images[4]}</div>
